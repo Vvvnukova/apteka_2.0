2ddf8534cba35699fba89a8e25be58acf379b73c
vvvnukova <veronica.vnukova@gmail.com>
2016-05-10 20:25:04 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/apteka_2.0/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/apteka_2.0/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/apteka_2.0/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/apteka_2.0/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/apteka_2.0/admin/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/apteka_2.0/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/apteka_2.0/admin/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/apteka_2.0/admin/view/template/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/apteka_2.0/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/apteka_2.0/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/upload/');
+define('DIR_CATALOG', '/Applications/MAMP/htdocs/apteka_2.0/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'mouse5');
+define('DB_DATABASE', 'apteka');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/apteka_2.0/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/apteka_2.0/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/apteka_2.0/catalog/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/apteka_2.0/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/apteka_2.0/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/apteka_2.0/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/apteka_2.0/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/apteka_2.0/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/apteka_2.0/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'mouse5');
+define('DB_DATABASE', 'apteka');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
